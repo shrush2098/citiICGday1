09ad1317de16fb1eb5fceaf4937653a6e719620a
shrush2098 <shrushti2098@gmail.com>
2020-09-16 11:36:58 +0000

    third commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,14 @@
-class Helloworld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!, Hi there !!!"); 
-    }
+public class Helloworld {
+
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
+	}
 }
+
 
 
